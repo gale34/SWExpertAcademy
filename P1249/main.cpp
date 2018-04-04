c1b6a2a53890df442d11afa6f712d641d5727087
Paul Je <dev.gales34@gmail.com>
2018-04-04 16:36:18 +0900

    20180404
    ...

--- P1249/main.cpp
+++ P1249/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <queue>
 
 using namespace std;
 
 int shortestPath(int** warMap, int** cache, int mapSize, int x, int y);
 
 #define INF 999999
+#define VISITED 1
+#define NOT_VISITED 0
 
 int pX[4] = {0,1,0,-1};
 int pY[4] = {1,0,-1,0};
@@ -59,8 +62,8 @@ int main()
 
         for(int i = 0; i < mapSize; i++)
         {
-             delete warMap[i];
-             delete cache[i];
+            delete warMap[i];
+            delete cache[i];
         }
         delete warMap;
         delete cache;
@@ -84,7 +87,7 @@ int shortestPath(int** warMap, int** cache, int mapSize, int x, int y)
         int nextY = y + pY[i];
 
         if(nextX >= 0 && nextX < mapSize &&
-           nextY >= 0 && nextY < mapSize)
+                nextY >= 0 && nextY < mapSize)
         {
             int target = shortestPath(warMap,cache,mapSize, nextX, nextY) + warMap[x][y];
             answer = min(answer,target);
@@ -93,4 +96,43 @@ int shortestPath(int** warMap, int** cache, int mapSize, int x, int y)
     }
 
     return answer;
+}
+
+int shortestPath2(int** warMap, int** cache, int mapSize, int x, int y)
+{
+    priority_queue<pair<int, pair<int, int> > > dijkstraQueue;
+
+    int** visited = new int*[mapSize];
+    for(int i = 0; i < mapSize; i++)
+    {
+        visited[i] = new int[mapSize];
+        memset(visited[i],NOT_VISITED,sizeof(int)*mapSize);
+    }
+
+    dijkstraQueue.push(make_pair(0,make_pair(0,0)));
+    cache[0][0] = 0;
+
+    while(!dijkstraQueue.empty())
+    {
+        pair<int, pair<int,int> > target = dijkstraQueue.top();
+        pair<int, int> targetPt = target.second;
+        dijkstraQueue.pop();
+        visited[targetPt.first][targetPt.second] = VISITED;
+
+        for(int i = 0; i < 4; i++)
+        {
+            int nextX = targetPt.first + pX[i];
+            int nextY = targetPt.second + pY[i];
+
+            if(nextX >= 0 && nextX < mapSize &&
+                    nextY >= 0 && nextY < mapSize && visited[nextX][nextY])
+            {
+                cache[nextX][nextY] = min(cache[nextX][nextY], cache[targetPt.first][targetPt.second] + warMap[nextX][nextY]);
+
+                int target = shortestPath(warMap,cache,mapSize, nextX, nextY) + warMap[x][y];
+                answer = min(answer,target);
+            }
+        }
+    }
+
 }
