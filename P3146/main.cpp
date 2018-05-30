14f31320aee380839e98a9374ae2f93ab66a9a5a
Paul Je <dev.gales34@gmail.com>
2018-05-30 17:04:37 +0900

    20180530
    ...

--- P3146/main.cpp
+++ P3146/main.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-#define MAX_VALUE 10000*3000
+#define MAX_VALUE 100000*3000
 
-pair<int,int> minChapters(vector<int>& chapters, vector<pair<int,int> >& cache, int start);
+void minChapters(int* accumChapters, int* minIndex, int** cache,int numSize);
 
 int main()
 {
@@ -20,56 +20,73 @@ int main()
         int k;
         cin >> k;
 
-        vector<int> chapters(k,0);
-        vector<pair<int,int> > cache(k, make_pair(MAX_VALUE,MAX_VALUE));
-        // first : sum of two value
-        // second : accum..value
+        int* accumChapters = new int[k+1];
+        int* minIndex = new int[k+1];
+        int** cache = new int*[k+1];
 
+        for(int i = 0; i <= k; i++)
+        {
+            accumChapters[i] = 0;
+            minIndex[i] = 0;
+
+            cache[i] = new int[k+1];
+            for(int j = 0; j <= k; j++)
+                cache[i][j] = MAX_VALUE;
+
+            cache[i][i] = MAX_VALUE;
+            if(i > 0)
+            {
+                cache[i][i-1] = 0;
+                cache[i-1][i] = 0;
+            }
+        }
+
+        for(int i = 1; i <= k; i++)
+        {
+            cin >> accumChapters[i];
+            accumChapters[i] += accumChapters[i-1];
+            minIndex[i-1] = i;
+        }
+
+        minChapters(accumChapters,minIndex,cache,k);
+
+        int answer = cache[0][k];
+
+        cout << "#" << t+1 << " " << answer << endl;
+
+        delete accumChapters;
+        delete minIndex;
         for(int i = 0; i < k; i++)
-            cin >> chapters[i];
+            delete cache[i];
+        delete cache;
 
-        pair<int,int> answer = minChapters(chapters, cache, 0);
-
-        for(int i = 0; i < cache.size(); i++)
-            cout << i << " : " << cache[i].second <<endl;
-
-        cout << "#" << t+1 << " " << answer.second << endl;
     }
 
     return 0;
 }
 
-pair<int,int> minChapters(vector<int>& chapters, vector<pair<int,int> >& cache, int start)
+void minChapters(int* accumChapters, int* minIndex, int** cache, int numSize)
 {
-    int chapterSize = chapters.size();
 
-    pair<int,int>& answer = cache[start];
-    if(start == chapterSize - 1)
-        answer.first = answer.second = chapters[start];
-    else if(start == chapterSize - 2)
-        answer.first = answer.second = chapters[start] + chapters[start+1];
-    else
+    for(int len = 2; len <= numSize; len++)
     {
-        if(answer.second == MAX_VALUE)
+        for(int i = 0, j = len; j <= numSize; i++, j++)
         {
-            pair<int,int> target1 = minChapters(chapters,cache,start+1);
-            pair<int,int> target2 = minChapters(chapters,cache,start+2);
+            int start = minIndex[i];
+            int finish = minIndex[i+1];
 
-            int sumTarget1 = chapters[start] + target1.first + target1.second;
-            int sumTarget2 = chapters[start] + chapters[start+1] + target2.first + target2.second;
-
-            if(sumTarget1 <= sumTarget2)
+            for(int k = start; k <= finish; k++)
             {
-                answer.first = chapters[start] + target1.first;
-                answer.second = sumTarget1;
+                int temp = cache[i][k] + cache[k][j];
+                if(cache[i][j] > temp)
+                {
+                    cache[i][j] = temp;
+                    minIndex[i] = k;
+                }
             }
-            else
-            {
-                answer.first = chapters[start] + chapters[start+1] + target2.first;
-                answer.second = sumTarget2;
-            }
+            cache[i][j] += accumChapters[j] - accumChapters[i];
         }
     }
 
-    return answer;
+    return ;
 }
